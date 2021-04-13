850a6b462d5bd420c096f40cff582491e8a696d7
hmtees <67087494+hmtees@users.noreply.github.com>
2021-04-13 00:23:40 -0400

    commit

--- forms/contact.php
+++ forms/contact.php
@@ -36,6 +36,7 @@
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
+  $contact->bcc = array('ultracmroberson@gmail.com', 'bccreceiver2@example.com');
 
   echo $contact->send();
 ?>
