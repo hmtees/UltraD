4c3871fb6fa52a78a492d3f5ff68ce375ab5ed12
Geoffrey D Agumya <jdagumya@gmail.com>
2021-04-22 16:49:29 -0700

    clean up

--- Signupjs.js
+++ Signupjs.js
@@ -120,34 +120,3 @@ function mainDriver(){
   let submitInfoBtn = document.getElementById("createAccountButton");
   submitInfoBtn.onclick = getInfoAndVerify;
 }
-
-/**
- * Returns true or false based on if the object values are correct
- * @param signUpObj
- */
-function signUpUser(firstname,lastname, email, pwd, re_pwd, school, gradyear) {
-  // Ensure password and re_password match
-  if (pwd != re_pwd) {
-    alert("Passwords do not match.");
-
-  } else {
-    // Register the user with the Firebase API (NOTE: auto logs in)
-    firebase.auth().createUserWithEmailAndPassword(email,pwd).then(function(userCredential) {
-      var db = firebase.firestore();
-      user = userCredential.User;
-      localStorage.userId = (user.uid);
-      console.log("New User: "+localStorage.userId);
-      console.log()
-      //There's a much better way to do this using node.js, but for now this at least works. 
-      db.collection('users').doc(user.uid).set({email: user.email})
-      db.collection('users').doc(user.uid).collection('Actions').doc('Surgery').set({Correct:0})
-      db.collection('users').doc(user.uid).collection('Actions').doc('CT Scan').set({Correct:0})
-      db.collection('users').doc(user.uid).collection('Actions').doc('Intervention').set({Correct:0})
-      db.collection('users').doc(user.uid).collection('Actions').doc('Observation').set({Correct:0}).then(
-          function(user) {window.location.replace("./Main%20UI.html")}
-          );
-    }, function(error) {
-      printErrorMessage(error, email, pwd, re_pwd)
-    })
-  }
-}
